Sharp336/grimoire-agent-engine

address codex round 65: close reopened managers when revival construction fails
- both subagent revival paths (task/executor.ts reviveSession and
  task/persisted-revive.ts) open a fresh SessionManager and pass it
  straight into createAgentSession; a factory rejection before any
  AgentSession is constructed (e.g. expected registry generation no
  longer available) leaves the newly registered live-pid owner claim
  with no cleanup path, so every later undo-tail gc in the long-lived
  parent skips that session until process exit. Both factory calls are
  now wrapped: on rejection the reopened manager is closed before
  rethrowing, releasing the claim
- regression test (persisted-revive): a real file-backed manager handed
  through SessionManager.open with createAgentSession rejecting leaves
  no owner claim in the sidecar once the reviver settles; proven to
  fail with the revival close neutered. The executor reviveSession
  guard is the same pattern; it is not independently exercisable
  without the full park/revive lifecycle in-process

packages/coding-agent/src/task/executor.ts
@@ -3243,22 +3243,31 @@ export async function runSubprocess(options: ExecutorOptions): Promise<SingleRes
 					if (options.parentArtifactManager) {
 						reopened.adoptArtifactManager(options.parentArtifactManager);
 					}
-					const { session: revived } = await createAgentSession(
-						buildSubagentSessionOptions(reopened, expectedAgentRef),
-					);
+					// The factory can reject before constructing an AgentSession
+					// (e.g. expected registry generation gone), and its error path
+					// does not dispose an externally supplied manager — close the
+					// reopened manager so its live-pid owner claim does not pin
+					// the session against undo-tail gc in the parent.
+					let session: AgentSession;
+					try {
+						({ session } = await createAgentSession(buildSubagentSessionOptions(reopened, expectedAgentRef)));
+					} catch (err) {
+						void reopened.close().catch(() => {});
+						throw err;
+					}
 					// Re-run the executor's extension wiring on the rebuilt session.
 					// Skipping it leaves the runner pre-init, so a `tool_call` handler
 					// touching a runtime action trips the fail-closed gate and blocks
-					// every tool (including `yield`) in the revived agent (issue #8824).
-					await initializeExtensions(revived, {
+					// every tool (including `yield`) in the session agent (issue #8824).
+					await initializeExtensions(session, {
 						reportSendError: (action, err) =>
 							logger.error("Extension send failed", { action, error: err.message }),
 						reportRuntimeError: err =>
 							logger.error("Extension error", { path: err.extensionPath, error: err.error }),
 					});
-					AgentRegistry.global().syncSessionStatus(id, revived);
-					installIrcWakeTurnMonitor(revived);
-					return revived;
+					AgentRegistry.global().syncSessionStatus(id, session);
+					installIrcWakeTurnMonitor(session);
+					return session;
 				};
 			}
 

can1357/oh-my-pi

packages/coding-agent/src/task/executor.ts
@@ -3930,22 +3930,33 @@ export async function runSubprocess(options: ExecutorOptions): Promise<SingleRes
 								AgentRegistry.global().get(MAIN_AGENT_ID)?.sessionFile,
 						);
 					}
-					const { session: revived } = await createAgentSession(
+				// The factory can reject before constructing an AgentSession
+				// (e.g. expected registry generation gone), and its error path
+				// does not dispose an externally supplied manager — close the
+				// reopened manager so its live-pid owner claim does not pin
+				// the session against undo-tail gc in the parent.
+				let revived: AgentSession;
+				try {
+					({ session: revived } = await createAgentSession(
 						buildSubagentSessionOptions(reopened, expectedAgentRef, true),
-					);
+					));
+				} catch (err) {
+					void reopened.close().catch(() => {});
+					throw err;
+				}
 					// Re-run the executor's extension wiring on the rebuilt session.
 					// Skipping it leaves the runner pre-init, so a `tool_call` handler
 					// touching a runtime action trips the fail-closed gate and blocks
-					// every tool (including `yield`) in the revived agent (issue #8824).
-					await initializeExtensions(revived, {
+					// every tool (including `yield`) in the session agent (issue #8824).
+					await initializeExtensions(session, {
 						reportSendError: (action, err) =>
 							logger.error("Extension send failed", { action, error: err.message }),
 						reportRuntimeError: err =>
 							logger.error("Extension error", { path: err.extensionPath, error: err.error }),
 					});
-					AgentRegistry.global().syncSessionStatus(id, revived);
-					installIrcWakeTurnMonitor(revived);
-					return revived;
+					AgentRegistry.global().syncSessionStatus(id, session);
+					installIrcWakeTurnMonitor(session);
+					return session;
 				};
 			}
 